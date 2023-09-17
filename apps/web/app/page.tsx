9e376b972958df2ab5ce68695f60b04166c5da54
Stanisław Synowiec <stasyn1410@gmail.com>
2023-09-17 14:36:05 +0200

    remove: button in server component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from '@nextui-org/react';
-
 const IndexPage = () => {
 	return (
 		<div>
 			<h1 className="text-red-500">Index Page</h1>
-			<Button />
+			{/*<Button />*/}
 		</div>
 	);
 };
