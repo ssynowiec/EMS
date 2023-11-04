5c7980b7eecb799214b7cd1d217a493386f1fa3f
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-04 20:03:23 +0100

    edit: edit page

--- apps/web/app/(dashboard)/users/[id]/edit/page.tsx
+++ apps/web/app/(dashboard)/users/[id]/edit/page.tsx
@@ -1,11 +1,31 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
 type EditUserPageProps = {
 	params: {
 		id: string;
 	};
 };
 
-const EditUserPage = ({ params }: EditUserPageProps) => {
-	return <div>User id: {params.id}</div>;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const EditUserPage = ({ params: { id } }: EditUserPageProps) => {
+	const query = useQuery({
+		queryKey: ['user', id],
+		queryFn: async () => {
+			const res = await fetch(`${API_URL}/user/${id}`);
+			return res.json();
+		},
+	});
+
+	const user = query.data || {};
+
+	return (
+		<div>
+			Edit user: <span className="font-bold">{user.name}</span>
+		</div>
+	);
 };
 
 export default EditUserPage;
