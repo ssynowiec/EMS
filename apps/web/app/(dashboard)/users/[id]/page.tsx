3d1d568adc96749a04c8961f1b567be0d1cd2674
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-04 20:03:06 +0100

    add: user details page

---
+++ apps/web/app/(dashboard)/users/[id]/page.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from 'next';
+import { Button, Link } from '@nextui-org/react';
+import { EditIcon } from 'ui';
+
+type UserPageProps = {
+	params: {
+		id: string;
+	};
+};
+
+type Props = {
+	params: { id: string };
+	searchParams: { [key: string]: string | string[] | undefined };
+};
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const getUser = async (userId: string) => {
+	const res = await fetch(`${API_URL}/user/${userId}`);
+
+	if (res.status === 404) return null;
+
+	return res.json();
+};
+
+export const generateMetadata = async ({
+	params,
+}: Props): Promise<Metadata> => {
+	const id = params.id;
+
+	const user = await getUser(id);
+
+	if (!user) return { title: 'User not found' };
+
+	return {
+		title: `User: ${user.name} details`,
+	};
+};
+
+const UserPage = async ({ params: { id } }: UserPageProps) => {
+	const user = await getUser(id);
+
+	if (!user)
+		return (
+			<section className="h-full w-full flex justify-center items-center flex-col">
+				<h1>User not found</h1>
+				<Button as={Link} href="/users" className="bg-purple text-white">
+					Back to all users
+				</Button>
+			</section>
+		);
+
+	return (
+		<>
+			<h1>
+				User: <span className="font-bold">{user.name}</span>
+			</h1>
+			<Button
+				href={`/users/${id}/edit`}
+				as={Link}
+				className="bg-purple text-white"
+				startContent={<EditIcon />}
+			>
+				Edit user
+			</Button>
+		</>
+	);
+};
+
+export default UserPage;
--- apps/web/components/usersTable/UsersTable.tsx
+++ apps/web/components/usersTable/UsersTable.tsx
@@ -192,9 +192,12 @@ export const UsersTable = ({ filters }: UsersTableProps) => {
 				return (
 					<div className="relative flex items-center gap-2">
 						<Tooltip content="Details">
-							<span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+							<Link
+								className="text-lg text-default-400 cursor-pointer active:opacity-50"
+								href={`/users/${user.id}`}
+							>
 								<EyeIcon />
-							</span>
+							</Link>
 						</Tooltip>
 						<Tooltip content="Edit user">
 							<Link
