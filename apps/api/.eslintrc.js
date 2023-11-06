1eac2c0ec3968b728d384ec5f19cd207476b9132
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-06 13:11:05 +0100

    fix: eslint

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
 	root: true,
-	extends: ['next/core-web-vitals', 'custom'],
-	rules: {
-		'@next/next/no-html-link-for-pages': 'off',
-		'react/jsx-key': 'off',
-		'react-hooks/exhaustive-deps': 'error',
-	},
+	extends: ['custom'],
+	// rules: {
+	// 	'@next/next/no-html-link-for-pages': 'off',
+	// 	'react/jsx-key': 'off',
+	// 	'react-hooks/exhaustive-deps': 'error',
+	// },
 };
