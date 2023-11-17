0c2f8fc5e49236ae8b496291765fe263843d5bc8
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-17 17:09:30 +0100

    fix: session type

--- apps/web/types/next-auth.d.ts
+++ apps/web/types/next-auth.d.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import NextAuth, { type DefaultSession, type DefaultUser } from 'next-auth';
+import { type DefaultSession, type DefaultUser } from 'next-auth';
 
 declare module 'next-auth' {
 	interface Session extends DefaultSession {
 		user: {
 			role: string | unknown;
-		};
+		} & DefaultSession['user'];
 	}
 
 	interface User extends DefaultUser {
