bb00e1d8a64752387b9353f83ce646154b423ab5
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-19 12:17:43 +0100

    fix: next-auth types

--- apps/web/types/next-auth.d.ts
+++ apps/web/types/next-auth.d.ts
@@ -4,11 +4,14 @@ import { type DefaultSession, type DefaultUser } from 'next-auth';
 declare module 'next-auth' {
 	interface Session extends DefaultSession {
 		user: {
+			id: string | unknown;
 			role: string | unknown;
 		} & DefaultSession['user'];
 	}
 
 	interface User extends DefaultUser {
+		id: string;
 		role: string;
+		status: string;
 	}
 }
