ee6ca81192d8642b1a370d949eef4a681edc3e75
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-20 14:20:51 +0100

    add: live preview event create

--- apps/web/components/addNewEventForm/AddNewEventForm.tsx
+++ apps/web/components/addNewEventForm/AddNewEventForm.tsx
@@ -2,6 +2,8 @@ import { Step1 } from '@/components/addNewEventForm/step1/Step1';
 import { Step2 } from '@/components/addNewEventForm/step2/Step2';
 import { useFormContext } from 'react-hook-form';
 import { env } from '../../env.d.mjs';
+import { EventCard } from '@/components/eventCard/EventCard';
+import { useState } from 'react';
 
 interface AddNewEventFormProps {
 	step: number;
@@ -76,7 +78,26 @@ const onSubmit = async (data: Inputs) => {
 };
 
 export const AddNewEventForm = ({ step }: AddNewEventFormProps) => {
-	const { handleSubmit } = useFormContext();
+	const { handleSubmit, watch } = useFormContext();
+	const [eventThumbnail, setEventThumbnail] = useState<
+		string | ArrayBuffer | null
+	>('');
+
+	const event = {
+		name: watch('eventName') || 'My event name',
+		slug: watch('eventUrl') || '',
+		thumbnail: eventThumbnail || '/thumb-placeholder.png',
+		description:
+			watch('eventDescription') ||
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac ligula in velit congue interdum eu nec turpis. Maecenas consequa pretium sem nec ultrices. Integer vel vulputate purus, ornare consequat dui. Cras laoreet sollicitudin mauris sed dictum. Sed in nisi id quam dapibus mattis vel at metus. Nam porttitor sapien augue, ut lacinia lacus sagittis sit amet. Vivamus mollis, ligula sed luctus molestie, urna odio hendrerit urna, sit amet egestas orci metus sed sem. Vestibulum cursus consequat leo, aliquam fermentum arcu fringilla non. Quisque ac tincidunt justo. Pellentesque tincidunt dui ipsum, vitae finibus purus varius at. Nulla dapibus fringilla risus, in sagittis justo semper nec. Etiam at rutrum est. Donec efficitur varius orci, quis bibendum felis tincidunt ut. Duis eget tempor justo. Phasellus eu imperdiet ex, at tempor nibh.',
+		startDate: watch('eventStartDate') || new Date(),
+		startTime: watch('eventStartTime') || '',
+		endDate: watch('eventEndDate') || new Date(),
+		endTime: watch('eventEndTime') || '',
+		eventType: watch('eventType') || 'hybrid',
+		onlineLocation: watch('eventOnlineLocation') || '',
+		offlineLocation: watch('eventLocation') || '',
+	};
 
 	return (
 		<>
@@ -88,12 +109,19 @@ export const AddNewEventForm = ({ step }: AddNewEventFormProps) => {
 				{(() => {
 					switch (step) {
 						case 1:
-							return <Step1 />;
+							return (
+								<Step1
+									eventThumbnail={eventThumbnail}
+									setEventThumbnail={setEventThumbnail}
+								/>
+							);
 						case 2:
 							return <Step2 />;
 					}
 				})()}
 			</form>
+			<p className="font-bold">Your event preview:</p>
+			<EventCard event={event} />
 		</>
 	);
 };
--- apps/web/components/eventCard/EventCard.tsx
+++ apps/web/components/eventCard/EventCard.tsx
@@ -1,21 +1,61 @@
 import { Card, CardBody, Image } from '@nextui-org/react';
 
 export const EventCard = ({ event }) => {
-	const { name, slug, thumbnail, description, date, location } = event;
+	const {
+		name,
+		thumbnail,
+		description,
+		startDate,
+		startTime,
+		endDate,
+		endTime,
+		eventType,
+		onlineLocation,
+		offlineLocation,
+	} = event;
+
+	const startDateString = new Date(startDate).toLocaleDateString('pl-PL');
+	const endDateString = new Date(endDate).toLocaleDateString('pl-PL');
+
+	const location =
+		eventType === 'online' ||
+		(onlineLocation !== null && offlineLocation === '')
+			? 'Online'
+			: eventType === 'offline' ||
+			    (onlineLocation === null && offlineLocation !== '')
+			  ? offlineLocation
+			  : `Online & ${offlineLocation}`;
 
 	return (
-		<li>
-			<a href={`/${slug}`}>
-				<Card>
-					<CardBody className="flex flex-row">
-						<Image src={thumbnail} alt={name} className="w-1/3" />
-						<div className="w-2/3">
-							<h2>{name}</h2>
-							<p>{description}</p>
-						</div>
-					</CardBody>
-				</Card>
-			</a>
-		</li>
+		<Card className="my-4">
+			<CardBody className="flex flex-row">
+				<div className="w-1/3 md:mr-4 max-h-full">
+					<Image
+						src={thumbnail}
+						alt={name}
+						width={1920}
+						height={1080}
+						className="max-h-full"
+					/>
+				</div>
+				<div className="w-2/3">
+					<h2 className="text-xl font-bold">{name}</h2>
+					<p>
+						{description && description.length > 255
+							? description.substring(0, 255) + '...'
+							: description}
+					</p>
+					<div>
+						<p>
+							{startDateString === endDateString
+								? startDateString
+								: `${startDateString} ${startTime} - ${endDateString} ${endTime}`}
+						</p>
+						{/*<CalendarIconWithDate date={startDate} />*/}
+						<p>{location}</p>
+					</div>
+				</div>
+			</CardBody>
+		</Card>
 	);
 };
