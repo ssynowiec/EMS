0cb167c43a16946b7563a3f9fa3b4b095a7ab796
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-21 12:35:23 +0100

    change: extend main dashboard layout in dashboard alyout & slug layout

---
+++ apps/web/app/my-event/[slug]/layout.tsx
@@ -0,0 +1,72 @@
+import { getEventBySlug } from '../../../utils/getEventBySlug';
+import { notFound } from 'next/navigation';
+import type { ReactNode } from 'react';
+import { MainDashboardLayout } from '@/components/mainDashboardLayout/MainDashboardLayout';
+import type { SidebarLink } from '@/types/sidebarLink';
+
+interface EventOrganizerLayoutProps {
+	children: ReactNode;
+	params: {
+		slug: string;
+	};
+}
+
+let mainSidebarLinks: SidebarLink[] = [
+	{
+		href: '/my-events',
+		title: '<- Back to events',
+	},
+];
+
+const EventOrganizerLayout = async ({
+	children,
+	params: { slug },
+}: EventOrganizerLayoutProps) => {
+	const event = await getEventBySlug(slug);
+
+	if (!event) notFound();
+
+	const dangerZoneLinks: SidebarLink[] = [
+		{ href: `./${event.slug}/delete`, title: 'Delete event', color: 'danger' },
+	];
+
+	if (event.status === 'DRAFT') {
+		mainSidebarLinks = [
+			{
+				href: '/my-events',
+				title: '<- Back to events',
+			},
+			{
+				href: `./${event.slug}/publish`,
+				title: 'Publish event',
+				color: 'purple',
+			},
+		];
+	} else {
+		mainSidebarLinks = [
+			{
+				href: '/my-events',
+				title: '<- Back to events',
+			},
+		];
+	}
+
+	const sidebarSections = [
+		{
+			title: '',
+			links: mainSidebarLinks,
+			permissions: ['ADMIN', 'USER'],
+		},
+		{
+			title: 'Danger Zone 🚫',
+			links: dangerZoneLinks,
+			permissions: ['ADMIN', 'USER'],
+		},
+	];
+
+	return (
+		<MainDashboardLayout menu={sidebarSections}>{children}</MainDashboardLayout>
+	);
+};
+
+export default EventOrganizerLayout;
--- apps/web/components/dashboardSidebar/DashboardSidebar.tsx
+++ apps/web/components/dashboardSidebar/DashboardSidebar.tsx
@@ -1,65 +1,13 @@
 'use client';
 
 import { Image, Link } from '@nextui-org/react';
-import type { ReactNode } from 'react';
-import { HomeIcon, PlusIcon, SettingsIcon } from 'ui';
 import { SidebarSection } from '../sidebarSection/SidebarSection';
-import { SidebarItem } from '../sidebarItem/SidebarItem';
 import { SidebarFooter } from '../sidebarFooter/sidebarFooter';
 import { useSession } from 'next-auth/react';
 import clsx from 'clsx';
 import { useSidebarContext } from 'app/(dashboard)/sidebarContext';
 
-export type SidebarLink = {
-	href: string;
-	title: string;
-	icon?: ReactNode;
-	color?: string;
-};
-
-const userSidebarLinks: SidebarLink[] = [
-	{
-		href: '/settings',
-		title: 'Settings',
-		icon: <SettingsIcon />,
-	},
-	{ href: '/statistics', title: 'Statistics' },
-	{ href: '/users', title: 'Users' },
-	{ href: '/Logout', title: 'Home' },
-];
-
-const mainSidebarLinks: SidebarLink[] = [
-	{
-		href: '/add-event',
-		title: 'Add Event',
-		icon: <PlusIcon />,
-		color: 'purple',
-	},
-];
-
-const organizerSidebarLinks: SidebarLink[] = [
-	{ href: '/my-events', title: 'My events' },
-];
-
-const sidebarSections = [
-	{
-		title: '',
-		links: mainSidebarLinks,
-		permissions: ['ADMIN', 'USER'],
-	},
-	{
-		title: 'Organizer',
-		links: organizerSidebarLinks,
-		permissions: ['ADMIN', 'USER'],
-	},
-	{
-		title: 'Admin',
-		links: userSidebarLinks,
-		permissions: ['ADMIN'],
-	},
-];
-
-export const DashboardSidebar = () => {
+export const DashboardSidebar = ({ menu }) => {
 	const session = useSession();
 	const { isSidebarOpen, closeSidebar } = useSidebarContext();
 	const userRole = session?.data?.user?.role;
@@ -82,8 +30,7 @@ export const DashboardSidebar = () => {
 			<div className="flex flex-col justify-between w-full flex-grow">
 				<div className="flex flex-col gap-6 px-2">
 					<div className="flex gap-2 flex-col">
-						<SidebarItem href="/dashboard" title="Home" icon={<HomeIcon />} />
-						{sidebarSections.map(
+						{menu.map(
 							(section) =>
 								section.permissions.includes(userRole) && (
 									<SidebarSection
