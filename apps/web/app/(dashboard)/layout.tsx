0cef0ca15e2f22b98d81b31d72dca2ea24f0e756
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-21 12:36:15 +0100

    change: extend main dashboard layout in dashboard layput

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,23 +1,62 @@
 import type { ReactNode } from 'react';
-import { DashboardNavbar } from '@/components/dashboardNavbar/DashboardNavbar';
-import { DashboardSidebar } from '@/components/dashboardSidebar/DashboardSidebar';
-import { SidebarProvider } from './sidebarContext';
+import { MainDashboardLayout } from '@/components/mainDashboardLayout/MainDashboardLayout';
+import { HomeIcon, PlusIcon, SettingsIcon } from 'ui';
+import { type SidebarLink } from '@/types/sidebarLink';
 
 type DashboardLayoutProps = {
 	children: ReactNode;
 };
 
+const userSidebarLinks: SidebarLink[] = [
+	{
+		href: '/settings',
+		title: 'Settings',
+		icon: <SettingsIcon />,
+	},
+	{ href: '/statistics', title: 'Statistics' },
+	{ href: '/users', title: 'Users' },
+	{ href: '/Logout', title: 'Home' },
+];
+
+const mainSidebarLinks: SidebarLink[] = [
+	{
+		href: '/dashboard',
+		title: 'Home',
+		icon: <HomeIcon />,
+	},
+	{
+		href: '/add-event',
+		title: 'Add Event',
+		icon: <PlusIcon />,
+		color: 'purple',
+	},
+];
+
+const organizerSidebarLinks: SidebarLink[] = [
+	{ href: '/my-events', title: 'My events' },
+];
+
+const sidebarSections = [
+	{
+		title: '',
+		links: mainSidebarLinks,
+		permissions: ['ADMIN', 'USER'],
+	},
+	{
+		title: 'Organizer',
+		links: organizerSidebarLinks,
+		permissions: ['ADMIN', 'USER'],
+	},
+	{
+		title: 'Admin',
+		links: userSidebarLinks,
+		permissions: ['ADMIN'],
+	},
+];
+
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
 	return (
-		<SidebarProvider>
-			<main className="flex h-screen">
-				<DashboardSidebar />
-				<div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-					<DashboardNavbar />
-					<section className="w-11/12 mx-auto">{children}</section>
-				</div>
-			</main>
-		</SidebarProvider>
+		<MainDashboardLayout menu={sidebarSections}>{children}</MainDashboardLayout>
 	);
 };
 
