04d14ddea3465b15b881df34de192471a9c15e80
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-22 10:25:07 +0100

    fix: cache in getEventBySlug fetch function

--- apps/web/utils/getEventBySlug.tsx
+++ apps/web/utils/getEventBySlug.tsx
@@ -1,6 +1,12 @@
 import { env } from '../env.d.mjs';
 
 export const getEventBySlug = async (slug: string) => {
-	const response = await fetch(`${env.NEXT_PUBLIC_API_URL}/event/${slug}`);
+	const response = await fetch(`${env.NEXT_PUBLIC_API_URL}/event/${slug}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		cache: 'no-cache',
+	});
 	return await response.json();
 };
