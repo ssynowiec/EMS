72c6d6e7b31a7ced97c0c4bbcde258fe7ffc5264
Stanisław Synowiec <stasyn1410@gmail.com>
2023-11-25 13:29:34 +0100

    add: card with progress circle

---
+++ apps/web/components/cardWithProgressCircle/CardWithProgressCircle.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { Flex, ProgressCircle, Text } from '@tremor/react';
+import clsx from 'clsx';
+import { Card, CardBody } from '@nextui-org/react';
+
+interface PercentageRegisteredUsersProps {
+	value: number;
+	maxValue: number;
+	title: string;
+}
+
+export const CardWithProgressCircle = ({
+	value,
+	maxValue,
+	title,
+}: PercentageRegisteredUsersProps) => {
+	const percentage = (value / maxValue) * 100;
+
+	return (
+		<Card className={clsx('w-full md:w-1/4 font-bold')} shadow="md">
+			<CardBody className="flex flex-row justify-between items-center">
+				<Flex className="space-x-5" justifyContent="center">
+					<ProgressCircle
+						value={percentage}
+						size="md"
+						strokeWidth={9}
+						showAnimation={true}
+					>
+						<span className="text-xs text-gray-700 font-medium">
+							{percentage}%
+						</span>
+					</ProgressCircle>
+					<div>
+						<Text className="font-medium text-gray-700">
+							{value}/{maxValue}
+						</Text>
+						<Text>{title}</Text>
+					</div>
+				</Flex>
+			</CardBody>
+		</Card>
+	);
+};
